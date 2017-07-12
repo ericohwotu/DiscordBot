d731b29a749215940be573caaf7ed8f8e1c839df
ericohwotu <eric.ohwotu@outlook.com>
2017-07-12 23:05:41 +0100

    added action validation for steam api

--- commands/api/steam-command.js
+++ commands/api/steam-command.js
@@ -8,13 +8,34 @@ module.exports = class SteamCommand extends Command {
             group: 'api',
             memberName: 'steam',
             description: 'runs the square in based on the words',
-            examples: ['steam']
+            examples: ['steam', 'hover'],
+            args: [
+                {
+                    key: 'action',
+                    prompt: 'what do you want to do?',
+                    validate: cmdValidate,
+                    type: 'string'
+                },
+                {
+                    key: 'search',
+                    prompt: 'input the game?',
+                    type: 'string'
+                }
+            ]
         });
     }
-    run(msg) {
+    run(msg, args) {
         // return msg.say(msg.content);
-        let commands = msg.content.replace('!bot steam ', '').trim()
-        return steamScript.run(commands, msg);
+        const { action, search } = args;
+        console.log(search)
 
+        return steamScript.run(action.toUpperCase(), search, msg);
     }
+
+
+}
+
+//ensure command is part of the approved commands
+function cmdValidate(value, msg, args) {
+    return value == "search" || value == "details"
 }
--- steam.js
+++ steam.js
@@ -3,30 +3,47 @@ const request = require("request");
 
 //use steam to get all the games
 
-function getGames(str, msg) {
+function getGames(cmd, str, msg) {
     let gamesJSON = [];
 
     let requestURL = "http://api.steampowered.com/ISteamApps/GetAppList/v0002";
 
     let r = request(requestURL, function (err, res, body) {
-        search(str, JSON.parse(body).applist.apps, msg);
+        search(cmd, str, JSON.parse(body).applist.apps, msg);
     });
 }
 
-function search(str, json, msg) {
+function search(cmd, str, json, msg) {
 
     let results = json.filter(function (obj) {
         return obj.name.toUpperCase().includes(str.toUpperCase());
     });
-    
+
     //sort by name length to better match 
-    results.sort(function(a,b){
+    results.sort(function (a, b) {
         return a.name.length - b.name.length;
     })
 
-    if(results[0]){
-        getGameDetails(results[0].appid, msg);
-    }else{
+    if (results[0]) {
+        if (cmd.includes("SEARCH")) {
+
+            let replyString = "";
+
+            results.forEach(function (obj) {
+                replyString += "\nName: " + obj.name;
+            });
+
+            if(replyString.length>2000){
+                replyString = replyString.slice(0,1900) + "\n..."
+            }
+            
+            msg.reply(replyString);
+
+        } else {
+            console.log(results)
+            getGameDetails(results[0].appid, msg);
+        }
+    } else {
         msg.reply("Sorry game not found.")
     }
 }
@@ -39,9 +56,12 @@ function getGameDetails(id, msg) {
     let r = request(requestURL, function (err, res, body) {
         let result = JSON.parse(body)[id].data;
         let regex = /<br\s*[\/]?>/gi
-        
-        msg.reply("\nName: " + result.name + " \nDescription: " + result.short_description.replace(regex,"\r\n") + " \nRelease Date: " +result.release_date.date + "\nMinimum Age: " + result.required_age);
-        console.log(result.name + " \n!!!d: " + result.short_description.replace(regex,"\r\n") + " \nrd: " +result.release_date.date + "\nra: " + result.required_age);
+        if (result) {
+            msg.reply("\nName: " + result.name + " \nDescription: " + result.short_description.replace(regex, "\r\n") + " \nRelease Date: " + result.release_date.date + "\nMinimum Age: " + result.required_age);
+            console.log(result.name + " \n!!!d: " + result.short_description.replace(regex, "\r\n") + " \nrd: " + result.release_date.date + "\nra: " + result.required_age);
+        } else {
+            msg.reply("sorry apparently the details cannot be found");
+        }
     });
 
     return gameResult;
@@ -50,7 +70,7 @@ function getGameDetails(id, msg) {
 
 
 module.exports = {
-    run : getGames
+    run: getGames
 };
 
 
