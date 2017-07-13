a52e06a638cc45d101c932eae09999ef142bb8bb
ericohwotu <eric.ohwotu@outlook.com>
2017-07-13 17:24:39 +0000

    updated tests

--- test/garage-test.js
+++ test/garage-test.js
@@ -1,4 +1,4 @@
-const garageSim = require('../commands/projects/garage.js')
+const garageSim = require('../commands/projects/garage.js');
 
 it("creating a vehicle should add the new vehicle to the array", function(){
     garageSim._run("create BENZ RY45KL 7 FAULTS");
@@ -7,7 +7,7 @@ it("creating a vehicle should add the new vehicle to the array", function(){
     if(x !== 1){
         throw new Error("Expected length: 1 Actual: " + x)
     }
-})
+});
 
 it("checking in a vehicle shoud fail before garage is open", function(){
 
@@ -17,7 +17,7 @@ it("checking in a vehicle shoud fail before garage is open", function(){
     if(x !== 0){
         throw new Error("Expected length: 0 Actual: " + x)
     }
-})
+});
 
 it("open should set the open state of the garage to true", function(){
 
@@ -26,5 +26,24 @@ it("open should set the open state of the garage to true", function(){
     
     if(!x){
         throw new Error("Expected garage.open: true Actual: " + x)
+    }
+});
+
+it("check in should increase the amount of vehicles in the garage",function(){
+    garageSim._run("check in RY45KL");
+    let x = garageSim._garage.vehicles.length
+    
+    if(x !== 1){
+        throw new Error("Expected length: 1 Actual: " + x)
+    }
+});
+
+it("close should set the open state of the garage to false", function(){
+
+    garageSim._run("close");
+    let x = garageSim._garage.open
+    
+    if(x){
+        throw new Error("Expected garage.open: false Actual: " + x)
     }
 })
